fa1e583ab64f5ce94b8c360c949ca58a059550fb
Benjamin <bhoogter@yahoo.com>
2019-11-18 12:53:40 -0500

    Create class-xml-file-test.php

---
+++ tests/class-xml-file-test.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+final class xml_file_test extends TestCase
+{
+    public function testContextLoads(): void
+    {
+        $this->assertTrue(true);
+    }
+}
