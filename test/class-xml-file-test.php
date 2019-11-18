4c3e3ea09ec3914844e52e30d7a298dd0f614ed3
Benjamin <bhoogter@yahoo.com>
2019-11-18 12:33:05 -0500

    Update class-xml-file-test.php

--- test/class-xml-file-test.php
+++ test/class-xml-file-test.php
@@ -1,3 +1,12 @@
-<?
+<?php
+declare(strict_types=1);
 
-?>
+use PHPUnit\Framework\TestCase;
+
+final class xml_file_test extends TestCase
+{
+    public function testContextLoads(): void
+    {
+        $this->assertTrue(true);
+    }
+}
