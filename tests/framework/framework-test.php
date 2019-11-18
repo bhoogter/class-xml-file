05348111be42958e7a66aae11fb407bcd549b14d
Benjamin <bhoogter@yahoo.com>
2019-11-18 18:49:33 -0500

    Update framework-test.php

--- tests/framework/framework-test.php
+++ tests/framework/framework-test.php
@@ -7,24 +7,6 @@ final class EmailTest extends TestCase
 {
     public function testCanBeCreatedFromValidEmailAddress(): void
     {
-        $this->assertInstanceOf(
-            Email::class,
-            Email::fromString('user@example.com')
-        );
-    }
-
-    public function testCannotBeCreatedFromInvalidEmailAddress(): void
-    {
-        $this->expectException(InvalidArgumentException::class);
-
-        Email::fromString('invalid');
-    }
-
-    public function testCanBeUsedAsString(): void
-    {
-        $this->assertEquals(
-            'user@example.com',
-            Email::fromString('user@example.com')
-        );
+        $this->assertTrue(true);
     }
 }
