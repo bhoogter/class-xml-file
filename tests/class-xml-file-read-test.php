74923355cbe49b8d5d5d00d0e1a2e142376c7bc9
Benjamin <bhoogter@yahoo.com>
2019-11-20 14:07:29 -0500

    Update class-xml-file-read-test.php

--- tests/class-xml-file-read-test.php
+++ tests/class-xml-file-read-test.php
@@ -18,21 +18,34 @@ final class xml_file_test extends TestCase
     function createTestXML() {
         $tmp = $this->tmpFile();
         $s = "";
-        $s += "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\" ?>
-        file
+        $s += "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\" ?>";
+        $s += "<items>" + "\n";
+        $s += "  <items id='1'>" + "\n";
+        $s += "    <name>Name #1</name>" + "\n";
+        $s += "    <size>Small</size>" + "\n";
+        $s += "  </item>" + "\n";
+        $s += "<items>" + "\n";
+        $s += "  <items id='2'>" + "\n";
+        $s += "    <name>Name #2</name>" + "\n";
+        $s += "    <size>Medium</size>" + "\n";
+        $s += "  </item>" + "\n";
+        $s += "<items>" + "\n";
+        $s += "  <items id='3'>" + "\n";
+        $s += "    <name>Name #3</name>" + "\n";
+        $s += "    <size>Large</size>" + "\n";
+        $s += "  </item>" + "\n";
+        $s += "</items>";
+        file_put_contents($tmp, $s);
+        return $tmp;
     }
     
     public function testReadXML(): void
     {
-        $subject = new xml_file();
-        $this->assertNotNull($subject);
-    }
-    public function testTempFile(): void
-    {
-        $testText = "12345";
-        $fname = $this->tmpFile();
-        file_put_contents($testText);
-        $result = file_get_contents($fname);
-        $this->assertEquals($testText, $result);
+        $tmp = $this->createTestXML();
+        $subject = new xml_file($tmp);
+        
+        $result = $subject->get("/items/item[id=2]/name");
+        
+        $this->assertEquals("Name #2", $result);
     }
 }
