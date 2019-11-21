6c3f958bd8da511b3ed8d16bcf99faf1e97fe9ab
Benjamin <bhoogter@yahoo.com>
2019-11-21 17:16:30 -0500

    Update class-xml-file-read-test.php

--- tests/class-xml-file-read-test.php
+++ tests/class-xml-file-read-test.php
@@ -33,7 +33,7 @@ final class xml_file_read_test extends xml_file_test
         $tmp = $this->createTestXML();
         $subject = new xml_file($tmp);
         
-        $result = $subject->get("/items/item[id=2]/name");
+        $result = $subject->get("/items/item[@id=2]/name");
         
         $this->assertEquals("Name #2", $result);
     }
