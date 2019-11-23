46f9f9d8d6616f623581d0207bc3118fce574dfe
Ben <bhoogter@ford.com>
2019-11-23 16:55:28 -0500

    File Loading Tests

--- tests/class-xml-file-read-test.php
+++ tests/class-xml-file-read-test.php
@@ -6,28 +6,7 @@ require(__DIR__."/class-xml-file-test.php");
 
 final class xml_file_read_test extends xml_file_test
 {
-    function createTestXML() {
-        $tmp = $this->tmpFile();
-        $s = "";
-        $s .= "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\" ?>\n";
-        $s .= "<items>" . "\n";
-        $s .= "  <item id='1'>" . "\n";
-        $s .= "    <name>Name #1</name>" . "\n";
-        $s .= "    <size>Small</size>" . "\n";
-        $s .= "  </item>" . "\n";
-        $s .= "  <item id='2'>" . "\n";
-        $s .= "    <name>Name #2</name>" . "\n";
-        $s .= "    <size>Medium</size>" . "\n";
-        $s .= "  </item>" . "\n";
-        $s .= "  <item id='3'>" . "\n";
-        $s .= "    <name>Name #3</name>" . "\n";
-        $s .= "    <size>Large</size>" . "\n";
-        $s .= "  </item>" . "\n";
-        $s .= "</items>";
-        file_put_contents($tmp, $s);
-        return $tmp;
-    }
-    
+
     public function testReadXML(): void
     {
         $tmp = $this->createTestXML();
--- tests/class-xml-file-test.php
+++ tests/class-xml-file-test.php
@@ -3,11 +3,12 @@ declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
 
-require(__DIR__."/../src/class-xml-file.php");
+require(__DIR__ . "/../src/class-xml-file.php");
 
 class xml_file_test extends TestCase
 {
     public $files;
+
     public function tearDown(): void
     {
         if (is_array($this->files))
@@ -19,6 +20,29 @@ class xml_file_test extends TestCase
         if ($this->files == null) $files = array();
         $this->files[] = $tmp = tempnam('/tmp', 'test_');
         unlink($tmp);
+        return $tmp;
+    }
+
+    function createTestXML()
+    {
+        $tmp = $this->tmpFile();
+        $s = "";
+        $s .= "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\" ?>\n";
+        $s .= "<items>" . "\n";
+        $s .= "  <item id='1'>" . "\n";
+        $s .= "    <name>Name #1</name>" . "\n";
+        $s .= "    <size>Small</size>" . "\n";
+        $s .= "  </item>" . "\n";
+        $s .= "  <item id='2'>" . "\n";
+        $s .= "    <name>Name #2</name>" . "\n";
+        $s .= "    <size>Medium</size>" . "\n";
+        $s .= "  </item>" . "\n";
+        $s .= "  <item id='3'>" . "\n";
+        $s .= "    <name>Name #3</name>" . "\n";
+        $s .= "    <size>Large</size>" . "\n";
+        $s .= "  </item>" . "\n";
+        $s .= "</items>";
+        file_put_contents($tmp, $s);
         return $tmp;
     }
 
@@ -36,5 +60,30 @@ class xml_file_test extends TestCase
         $result = file_get_contents($fname);
         $this->assertEquals($testText, $result);
     }
+
+    public function testLoadAFile(): void
+    {
+        $subject = new xml_file($fname = $this->createTestXML());
+        $this->assertNotEquals("", $subject->gid);
+        $this->assertEquals($fname, $subject->filename);
+        $this->assertTrue($subject->loaded);
+    }
+
+    public function testClearObject(): void
+    {
+        $subject = new xml_file($fname = $this->createTestXML(), "readonly,notidy");
+        $this->assertTrue($subject->loaded);
+        $this->assertTrue($subject->readonly);
+        $this->assertTrue($subject->notidy);
+        $gid = $subject->gid;
+
+        $subject->clear();
+        $this->assertFalse($subject->loaded);
+        $this->assertEquals("", $subject->filename);
+        $this->assertEquals($gid, $subject->gid);
+        $this->assertFalse($subject->readonly);
+        $this->assertFalse($subject->notidy);
+    }
+
 
 }
