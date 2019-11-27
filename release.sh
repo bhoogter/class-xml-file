4412bc60b218960fecd008356975e871d7cff6f0
Ben <bhoogter@ford.com>
2019-11-27 17:05:11 -0500

    0.2.0 dev

---
+++ release.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Remove '-dev' from the version file to prepare for release.
+sed -e 's/-dev$//' VERSION > VERSION.tmp
+mv -f VERSION.tmp VERSION
+
+# Tag a release
+ver="$(cat VERSION)"
+git add VERSION
+git commit -m "Version $ver"
+git tag "$ver"
+git push origin "$ver"
+
+# Advance to the next patch release, add the '-dev' suffix
+# back on, and commit the result.
+a=( ${ver//./ } ) && ((a[2]++))
+echo "${a[0]}.${a[1]}.${a[2]}-dev" > VERSION
+git add VERSION
+git commit -m "Back to -dev"
+git push origin master
