cb34e83db03f337b11ca4147eafadc58afcd3eb7
Ben <bhoogter@ford.com>
2020-01-17 18:25:24 -0500

    Smarten stub

--- src/stub.php
+++ src/stub.php
@@ -1,6 +1,9 @@
 <?php
 
 spl_autoload_register(function ($name) {
-    if ("xml_file" == $name) require_once("phar://" . __FILE__  . "/src/class-xml-file.php");
+    if ("xml_file" == $name) {
+        $f = (strpos(__FILE__, ".phar") === false ? __DIR__ : "phar://" . __FILE__ . "/src") . "/class-xml-file.php";
+        require_once($f);
+    }
 });
 __HALT_COMPILER();
