e97b7fad2b842c56bbc0eb6e44e243d2289b2a96
ben <bhoogter@yahoo.com>
2020-05-16 14:26:46 -0400

    json fix

--- src/class-xml-file.php
+++ src/class-xml-file.php
@@ -853,44 +853,77 @@ class xml_file extends xml_file_base
 
     public static function saveJsonXsltStandard() {
         return <<<'EOC'
-<?xml version="1.0"?>
+<?xml version="1.0" encoding="UTF-8" ?>
 <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
-    <xsl:output method="text"/>
+    <xsl:output method="text" encoding="utf-8"/>
     <xsl:variable name='q'>"</xsl:variable>
-
-    <xsl:template match="/">{
-        <xsl:apply-templates select="*"/>}
+    
+    <xsl:template match="/*[node()]">
+        <xsl:text>{</xsl:text>
+        <xsl:apply-templates select="." mode="detect" />
+        <xsl:text>}</xsl:text>
     </xsl:template>
-
-    <!-- Object or Element Property-->
-    <xsl:template match="*">
-        <xsl:value-of select='concat($q, name(), $q, ": ")' /><xsl:call-template name="Properties"/>
-    </xsl:template>
-
-    <!-- Array Element -->
-    <xsl:template match="*" mode="ArrayElement">
-        <xsl:call-template name="Properties"/>
-    </xsl:template>
-
-    <!-- Object Properties -->
-    <xsl:template name="Properties">
-        <xsl:variable name="childName" select="name(*[1])"/>
+    
+    <xsl:template match="*" mode="detect">
         <xsl:choose>
-            <xsl:when test="not(*|@*)"><xsl:value-of select='concat($q, ., $q)' /></xsl:when>
-            <xsl:when test="count(*[name()=$childName]) > 1"><xsl:value-of select='concat("{ ", $q, $childName, $q, ": ")' /> [<xsl:apply-templates select="*" mode="ArrayElement"/>] }</xsl:when>
-            <xsl:otherwise>{
-                <xsl:apply-templates select="@*"/>
-                <xsl:apply-templates select="*"/>
-    }</xsl:otherwise>
+            <xsl:when test="name(preceding-sibling::*[1]) = name(current()) and name(following-sibling::*[1]) != name(current())">
+                    <xsl:apply-templates select="." mode="obj-content" />
+                <xsl:text>]</xsl:text>
+                <xsl:if test="count(following-sibling::*[name() != name(current())]) &gt; 0">, </xsl:if>
+            </xsl:when>
+            <xsl:when test="name(preceding-sibling::*[1]) = name(current())">
+                    <xsl:apply-templates select="." mode="obj-content" />
+                    <xsl:if test="name(following-sibling::*) = name(current())">, </xsl:if>
+            </xsl:when>
+            <xsl:when test="following-sibling::*[1][name() = name(current())]">
+                <xsl:text>"</xsl:text><xsl:value-of select="name()"/><xsl:text>" : [</xsl:text>
+                    <xsl:apply-templates select="." mode="obj-content" /><xsl:text>, </xsl:text>
+            </xsl:when>
+            <xsl:when test="count(./child::*) > 0 or count(@*) > 0">
+                <xsl:value-of select='concat($q, name(), $q, ": ")' /><xsl:apply-templates select="." mode="obj-content" />
+                <xsl:if test="count(following-sibling::*) &gt; 0">, </xsl:if>
+            </xsl:when>
+            <xsl:when test="count(./child::*) = 0">
+                <xsl:value-of select='concat($q, name(), $q, ": ", $q, ., $q)' />
+                <xsl:if test="count(following-sibling::*) &gt; 0">, </xsl:if>
+            </xsl:when>
         </xsl:choose>
-        <xsl:if test="following-sibling::*">,</xsl:if>
     </xsl:template>
+    
+    <xsl:template match="*" mode="obj-content">
+        <xsl:text>{</xsl:text>
+            <xsl:apply-templates select="@*" mode="attr" />
+            <xsl:if test="count(@*) &gt; 0 and (count(child::*) &gt; 0 or text())">, </xsl:if>
+            <xsl:apply-templates select="./*" mode="detect" />
+            <xsl:if test="count(child::*) = 0 and text() and not(@*)">
+                <xsl:value-of select='concat($q, name(), $q, ": ", $q, text(), $q)' />
+            </xsl:if>
+            <xsl:if test="count(child::*) = 0 and text() and @*">
+                <xsl:value-of select='concat($q, "text", $q, ": ", $q, text(), $q)' />
+            </xsl:if>
+        <xsl:text>}</xsl:text>
+        <xsl:if test="not(position() &gt;= last())">, </xsl:if>
 
-    <!-- Attribute Property -->
-    <xsl:template match="@*">
-        <xsl:value-of select='concat($q, "@", name(), $q, ": ", $q, ., $q)' />
-        <xsl:if test="following-sibling::*">,</xsl:if>
     </xsl:template>
+    
+    <xsl:template match="@*" mode="attr">
+        <xsl:value-of select='concat($q, name(), $q, ": ", $q, ., $q)' />
+        <xsl:if test="not(position() &gt;= last())">,</xsl:if>
+    </xsl:template>
+    
+    <xsl:template match="node/@TEXT | text()" name="removeBreaks">
+        <xsl:param name="pText" select="normalize-space(.)"/>
+        <xsl:choose>
+            <xsl:when test="not(contains($pText, '&#xA;'))"><xsl:copy-of select="$pText"/></xsl:when>
+            <xsl:otherwise>
+                <xsl:value-of select="concat(substring-before($pText, '&#xD;&#xA;'), ' ')"/>
+                <xsl:call-template name="removeBreaks">
+                    <xsl:with-param name="pText" select="substring-after($pText, '&#xD;&#xA;')"/>
+                </xsl:call-template>
+            </xsl:otherwise>
+        </xsl:choose>
+    </xsl:template>
+    
 </xsl:stylesheet>
 EOC;
     }
@@ -929,7 +962,7 @@ EOC;
     public function saveJson($mode = '') {
         $str = self::docXml(self::transformXMLXSL_static($this->saveXML(), self::saveJsonStylesheet($mode)));
         $str = str_replace('<?xml version="1.0"?>', '', $str);
-        $str = str_replace("\n", '', $str);
+        // $str = str_replace("\n", '', $str);
         $result = self::tidyJson_string($str);
         return $result !== false ? $result : $str;  // in case tidy fails
    }
