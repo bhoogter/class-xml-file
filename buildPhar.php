ae437ef886d21a0155651abcc795eac8c2f0f689
ben <bhoogter@yahoo.com>
2022-01-04 17:20:30 -0500

    Fix phar pathing

--- buildPhar.php
+++ buildPhar.php
@@ -2,8 +2,12 @@
 
 unlink('xml-file.phar');
 $phar = new Phar('xml-file.phar');
-// $phar->buildFromDirectory('.', '/^src\/.*/');
-$phar->buildFromDirectory('.', '/^src\/.*$/');
+// $phar->buildFromDirectory('src');
+// $phar->buildFromIterator(new RecursiveIteratorIterator(new RecursiveDirectoryIterator('src')));
+$pattern = '/' . dirname(__FILE__) . '.src.*$/';
+$pattern = str_replace("\\", "\\\\", $pattern);
+print("PATTERN: $pattern");
+$phar->buildFromDirectory(dirname(__FILE__), $pattern);
 $phar->addFile("LICENSE");
 $phar->addFile("VERSION");
 $phar->addFile("README.md");
