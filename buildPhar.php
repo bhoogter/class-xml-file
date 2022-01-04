63dd61853edd155bbc6d030a25eb3d9ab498d8b6
ben <bhoogter@yahoo.com>
2022-01-04 16:39:31 -0500

    Fix packaging

--- buildPhar.php
+++ buildPhar.php
@@ -1,7 +1,9 @@
 <?php
 
+unlink('xml-file.phar');
 $phar = new Phar('xml-file.phar');
-$phar->buildFromDirectory('src/');
+// $phar->buildFromDirectory('.', '/^src\/.*/');
+$phar->buildFromDirectory('.', '/^src\/.*$/');
 $phar->addFile("LICENSE");
 $phar->addFile("VERSION");
 $phar->addFile("README.md");
