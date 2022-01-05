74279b3e2f21130a058443a23f3b3495331a9873
ben <bhoogter@yahoo.com>
2022-01-05 12:56:24 -0500

    Update test runner and stub

--- buildPhar.php
+++ buildPhar.php
@@ -2,11 +2,8 @@
 
 unlink('xml-file.phar');
 $phar = new Phar('xml-file.phar');
-// $phar->buildFromDirectory('src');
-// $phar->buildFromIterator(new RecursiveIteratorIterator(new RecursiveDirectoryIterator('src')));
 $pattern = '#' . dirname(__FILE__) . '.src.*$#';
 $pattern = str_replace("\\", "\\\\", $pattern);
-print("PATTERN: $pattern");
 $phar->buildFromDirectory(dirname(__FILE__), $pattern);
 $phar->addFile("LICENSE");
 $phar->addFile("VERSION");
--- src/stub.php
+++ src/stub.php
@@ -1,9 +1,12 @@
 <?php
 
 spl_autoload_register(function ($name) {
-    $d = (strpos(__FILE__, ".phar") === false ? __DIR__ : "phar://" . __FILE__ . "/src");
-    if ($name == "xml_file") require_once($d . "/class-xml-file.php");
-    if ($name == "xml_file_base") require_once($d . "/class-xml-file-base.php");
-    if ($name == "xml_file_interface") require_once($d . "/class-xml-file-interface.php");
+    // $d = (strpos(__FILE__, ".phar") === false ? __DIR__ : "phar://" . __FILE__ . "/src");
+    $d = __DIR__;
+    switch($name) {
+        case "xml_file": require_once($d . "/class-xml-file.php"); break;
+        case "xml_file_base": require_once($d . "/class-xml-file-base.php"); break;
+        case "xml_file_interface": require_once($d . "/class-xml-file-interface.php"); break;
+    }    
 });
 __HALT_COMPILER();
